3d9cbcd22141d1cf5d67dc57c3ff7ded3602bd0b
P.E <snow.ma.tree@gmail.com>
2021-12-26 08:01:18 +0900

    wip: カウントアップの処理でエラー発生→調査中

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>カウント：{count}</p>
+      <Button btn_click{...() => { setCount(count + 1) }} btn_txt="クリックしてね" />
     </div>
   );
+}
+
+function Button(props) {
+  return (
+    <button onClick={props.btn_click}>{props.btn_txt}</button>
+  )
 }
 
 export default App;
